6330578272436dcb6675bb9b5c983b99f4e34abb
Marco Torres <antoniomtz2001torres10ocram@gmail.com>
2023-05-27 17:20:56 -0600

    Reubicacion
    Se acomodó la posición del huevo

--- Proyecto Final Compu Grafica/ProyectoFinal.cpp
+++ Proyecto Final Compu Grafica/ProyectoFinal.cpp
@@ -1783,7 +1783,7 @@ int main()
 		/// </summary>
 		model = glm::mat4(1.0);
 		model = glm::scale(model, glm::vec3(0.65f, 0.65f, 0.65f));
-		model = glm::translate(model, glm::vec3(220.0f, 0.0f, -260.0f));
+		model = glm::translate(model, glm::vec3(180.0f, 0.0f, -260.0f));
 		model = glm::scale(model, glm::vec3(2.5f, 2.5f, 2.5f));
 		model = glm::rotate(model, 90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
