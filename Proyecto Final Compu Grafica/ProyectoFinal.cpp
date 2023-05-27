1fd1f1de694619112263418afdb64cc01bfbfd4d
CarlosCERS <101842946+CarlosCERS@users.noreply.github.com>
2023-05-27 13:39:59 -0600

    Commit Sillas menos

--- Proyecto Final Compu Grafica/ProyectoFinal.cpp
+++ Proyecto Final Compu Grafica/ProyectoFinal.cpp
@@ -1704,7 +1704,9 @@ int main()
 		/// Cartel en construccion.
 		/// </summary>
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(55.0f, 25.f, -101.0f));
+		model = glm::translate(model, glm::vec3(-140.0f, 0.f, 120.0f));
+		model = glm::rotate(model, -90 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f));
+		model = glm::rotate(model, 35 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f));
 		model = glm::scale(model, glm::vec3(15.f, 15.f, 1.f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		CartelConstruccion.UseTexture();
