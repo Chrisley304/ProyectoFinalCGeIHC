df73592b7ec0be5b6e71214fab50431aae2d863d
Christian Leyva <chrisley304@hotmail.com>
2023-06-10 16:46:17 -0600

    Implementacion de Audio con SFML

--- Proyecto Final Compu Grafica/ProyectoFinal.cpp
+++ Proyecto Final Compu Grafica/ProyectoFinal.cpp
@@ -16,6 +16,9 @@ Proyecto final de Computación Gráfica e Iteracción Humano-Computadora.
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+// Librería de Audio
+#include <SFML/Audio.hpp>
+
 #include "Camera.h"
 #include "Mesh.h"
 #include "Model.h"
@@ -137,6 +140,12 @@ bool cambioSkybox = false;
 bool noche = false;
 float horaDia = 0.0f;
 /********************** Fin Skybox **************************/
+
+/********************** Audio **************************/
+sf::Sound sonido;
+sf::SoundBuffer sound_buffer;
+sf::Music musica;
+/********************** Fin Audio **************************/
 
 /// <summary>
 /// Materiales.
@@ -1109,6 +1118,22 @@ int main()
     float giroAvatarZ = 0.0f;
 
 	bool *keys;
+    
+    sound_buffer.loadFromFile("Sounds/OOT_YoungLink_Attack1.wav");
+    
+    sf::Listener::setPosition((float)0.0f, (float)0.0f, (float)0.0f); // More detail about these two lines in the next tutorial sound.setPosition(0.Of, 0.Of, 0.Of);
+    sonido.setPitch(1.0f);
+    sonido.setVolume(7.0f);
+    sonido.setBuffer(sound_buffer);
+    sonido.setMinDistance(5.0f);
+    sonido.setAttenuation(0.5f);
+    sonido.setLoop(true);
+    sonido.play();
+    
+//    musica.openFromFile("The Trapezist - Quincas Moreira.ogs");
+//    musica.setVolume(100.0f);
+//    musica.setPitch(1.0f);
+//    musica.play();
 
 	/******************* Fin Animaciones Inicializacion ****************************/
 
